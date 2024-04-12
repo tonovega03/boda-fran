506d489034bc03cee355443fb3b38c00274a717a
COOPE\luivega <luivega@coopenae.fi.cr>
2024-04-12 11:14:24 -0600

    probar plantilla

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,17 +115,17 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
+                year: 2025,
+                month: 5,
                 day: 28,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
                 words: {
-                    days: 'day',
-                    hours: 'hour',
-                    minutes: 'minute',
-                    seconds: 'second',
+                    days: 'día',
+                    hours: 'hora',
+                    minutes: 'minuto',
+                    seconds: 'segundo',
                     pluralLetter: 's'
                 },
                 plural: true,
